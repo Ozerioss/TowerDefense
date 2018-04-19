6f0601bcd6b2bd23eb17746c31af292f8a8c325a
Karim Kidiss <kkidiss@yahoo.fr>
2018-04-19 18:46:13 +0200

    Added script to spawn waves

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveCountdownText;
+    public float wavesTimer = 5f;
+
+    private float countdown = 2f;
+    private int waveNumber = 1;
+
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = wavesTimer;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        Debug.Log("Wave spawned");
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveNumber++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
+    
