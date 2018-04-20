8e7f1e823e75273688c9af837efc2ea4503610a9
Karim Kidiss <kkidiss@yahoo.fr>
2018-04-20 11:54:44 +0200

    Adds shooting mechanic

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Transform target;
+
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if(target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+	}
+
+
+    void HitTarget()
+    {
+        Debug.Log("Hit ! ");
+        GameObject effectInstance = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, .5f); // Destroys the effect after 1 second
+
+        Destroy(target.gameObject); // temp destroy enemy
+
+        Destroy(gameObject); // Destroys the bullet
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,15 +4,24 @@ public class Turret : MonoBehaviour {
 
     private Transform target;
 
+    [Header("Turret attributes")]
+
     public float range = 10f;
+    public float fireRate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup")]
+
     public string enemytag = "Enemy";
     public Transform partToRotate;
     public int lockSpeed = 5;
 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Start()
     {
-        InvokeRepeating("getTarget", 0f, 0.5f);
+        InvokeRepeating("GetTarget", 0f, 0.5f);
     }
 
     void Update()
@@ -25,15 +34,34 @@ public class Turret : MonoBehaviour {
         Vector3 dir = target.position - transform.position;  // get direction to look at
         Quaternion lookRotation = Quaternion.LookRotation(dir);
 
-        //Vector3 rotation = lookRotation.eulerAngles; //conversion to euler angles
-
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * lockSpeed).eulerAngles;
 
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
+
+        if(fireCountdown <= 0f)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
     }
 
-    void getTarget()
+    void Shoot()
+    {
+        Debug.Log("Shoot !");
+        GameObject bulletGameObject = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGameObject.GetComponent<Bullet>();
+
+        if(bullet !=null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    void GetTarget()
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemytag); // stores all enemies in scene in the gameobject array
 
