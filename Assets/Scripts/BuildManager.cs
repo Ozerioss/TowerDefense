ee2d6a08b2a3e6080b68789df075b59c9a06253a
Karim Kidiss <kkidiss@yahoo.fr>
2018-04-20 12:42:05 +0200

    Adds ability to build turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    // Singleton pattern
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    private GameObject turretToBuild;
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private Renderer _rend;
+    private Color initialColor;
+    private GameObject turret;
+
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>(); //cashes the mesh renderer
+        initialColor = _rend.material.color; // stores initial color of node
+    }
+
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Tower already built on node");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+    // Callback method from unity engine
+    void OnMouseEnter()
+    {
+        _rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        _rend.material.color = initialColor;
+    }
+
+}
